e08b58a5a43e87e70ba40a437a6b01795bb01fa5
vbrdnk <burdenyuchok@gmail.com>
2019-11-04 23:35:04 +0200

    add header component with navigation links

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop-page/shop-page.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
