fa44cc9c379c13307e28da0d68fb8b12b4ed4da0
vbrdnk <burdenyuchok@gmail.com>
2019-11-09 23:30:04 +0200

    store user in a database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,7 +15,32 @@ const config = {
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-export const firesotre = firebase.firestore();
+export const firestore = firebase.firestore();
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('Error creating user', error);
+    }
+  }
+
+  return userRef;
+};
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
