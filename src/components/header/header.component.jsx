059b6840b08f5285ff7282af4cb69bf053cddf34
vbrdnk <burdenyuchok@gmail.com>
2019-11-30 20:57:51 +0200

    added addItem, removeItem functionality for checkout page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -30,7 +30,7 @@ const Header = ({currentUser, hidden}) => (
         currentUser ? (
         <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
           ) : (
-        <Link className='option' to='/signin'>SING IN</Link>
+        <Link className='option' to='/signin'>SIGN IN</Link>
         )
       }
       <CartIcon />
