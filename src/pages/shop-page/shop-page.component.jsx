b51d1981ec2cd54eb8ec06aea594cdc31b9ac088
vbrdnk <burdenyuchok@gmail.com>
2019-12-01 14:16:19 +0200

    add redux to shop page and directory page components

--- src/pages/shop-page/shop-page.component.jsx
+++ src/pages/shop-page/shop-page.component.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import {createStructuredSelector} from 'reselect';
+import {selectCollections} from '../../redux/shop/shop.selectors';
 
-import SHOP_DATA from './shop.data';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
+const ShopPage = ({collections})  => (
+  <div>
+    {
+      collections.map(({id, ...otherCollectionProps}) => (
+        <CollectionPreview key={id} {...otherCollectionProps}/>
+      ))
     }
-  }
+  </div>
+);
 
-  render() {
-    const {collections} = this.state;
-    return (
-      <div>
-        {
-          collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
-      </div>
-    )
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
