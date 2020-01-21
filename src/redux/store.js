38f0e28f923cb9c79622cd1aa43d472d6f5208bb
vbrdnk <burdenyuchok@gmail.com>
2020-01-21 13:55:22 +0200

    add reguxs-saga, refactor correspnding logic to use sagas for async events

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,22 @@
 import {createStore, applyMiddleware} from 'redux';
 import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import {fetchCollectionsStart} from './shop/shop.saga';
 
 import rootReducer from './root-reducer';
 
+const sagaMiddleware = createSagaMiddleware();
+console.log(sagaMiddleware);
 
-const middlewares = [logger, thunk];
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
+
+sagaMiddleware.run(fetchCollectionsStart);
